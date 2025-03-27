fdada9e6eb74949ee04ee6e31e90eb6a8ef9b8d7
Niklas Droste <nd@kaufmann.digital>
2025-03-27 11:15:41 +0100

    WIP: composer script

--- Classes/Composer/ComposerScripts.php
+++ Classes/Composer/ComposerScripts.php
@@ -10,15 +10,9 @@ use Composer\Script\Event;
 class ComposerScripts
 {
 
-    public static function postPackageInstall(PackageEvent $event)
+    public static function postPackageUpdateAndInstall()
     {
-        $composer = $event->getComposer();
-
-        $package = $composer->getRepositoryManager()->findPackage('spatie/mjml-php', '*');
-        $packagePath = $composer->getInstallationManager()->getInstallPath($package);
-
-        var_dump(shell_exec(sprintf('cd %s && npm install', $packagePath)));
-
+        \Neos\Flow\var_dump("die");die();
     }
 
 }
