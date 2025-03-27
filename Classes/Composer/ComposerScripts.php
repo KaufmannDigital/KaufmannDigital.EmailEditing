4a37dd919d9c853b73f57e561a7ade94e1aa2fa6
Niklas Droste <nd@kaufmann.digital>
2025-03-27 11:55:50 +0100

    Feature: automatic npm install with composer

--- Classes/Composer/ComposerScripts.php
+++ Classes/Composer/ComposerScripts.php
@@ -3,16 +3,23 @@ declare(strict_types=1);
 
 namespace KaufmannDigital\EmailEditing\Composer;
 
-
 use Composer\Installer\PackageEvent;
-use Composer\Script\Event;
 
 class ComposerScripts
 {
 
-    public static function postPackageUpdateAndInstall()
+    public static function mjmlNpmInitialisation(PackageEvent $event)
     {
-        \Neos\Flow\var_dump("die");die();
+        $package = $event->getComposer()->getRepositoryManager()->findPackage('spatie/mjml-php', '*');
+        $packagePath = $event->getComposer()->getInstallationManager()->getInstallPath($package);
+        $commandResult = shell_exec(sprintf('cd %s && npm install', $packagePath));
+
+        if (is_string($commandResult)) {
+            echo "🥳 Successfully installed MJML Node-Modules for package spatie/mjml-php" . PHP_EOL;
+        } else {
+            throw new \Exception('Failed to install MJML Node-Modules');
+        }
+
     }
 
 }
